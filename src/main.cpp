d91dc8904fcb0f2f6cc21b31ff1578ba44b685eb
cherif <benfetimacherif@gmail.com>
2025-01-17 15:54:19 +0100

    ajout hacheur

--- src/main.cpp
+++ src/main.cpp
@@ -1,93 +1,142 @@
-#include <Arduino.h>
+// Pilotage des phases (sorties)
+#define Hin_U 13
+#define Lin_U 12
+#define Hin_V 11
+#define Lin_V 10
+#define Hin_W 9
+#define Lin_W 8
+
+// Acq Hall
+#define capteur_a 7
+#define capteur_b 6
+#define capteur_c 5
+
+// pilotage alpha
+#define pin_alpha 4
 
 
-#define Ha 10
-#define Hb 9
-#define Hc 8
+#define PAS_ALPHA 5
 
-#define TLin_U 5
-#define TLin_V 6
-#define TLin_W 7
-
-#define THin_U 2
-#define THin_V 3
-#define THin_W 4
-
-bool a, b,c;
-bool kl_U, kh_U, kl_V, kh_V, kl_W, kh_W;
+bool a, b, c;
+bool K1, K2, K3, K4, K5, K6;
 
 long timestamp;
-
-
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(2000000);
-  pinMode(Ha, INPUT);
-  pinMode(Hb, INPUT);
-  pinMode(Hc, INPUT);
+// put your setup code here, to run once:
+// COnfiguration des phases à piloter
+pinMode(Hin_U, OUTPUT);
+pinMode(Lin_U, OUTPUT);
+pinMode(Hin_V, OUTPUT);
+pinMode(Lin_V, OUTPUT);
+pinMode(Hin_W, OUTPUT);
+pinMode(Lin_W, OUTPUT);
+
+// Configuration de l'acq. des capteurs Hall
+pinMode(capteur_a, INPUT);
+pinMode(capteur_b, INPUT);
+pinMode(capteur_c, INPUT);
+
+// Configuration
+pinMode(pin_alpha, OUTPUT);
 
 
-  pinMode(TLin_U,OUTPUT);
-  pinMode(TLin_V,OUTPUT);
-  pinMode(TLin_W,OUTPUT);
-  
-  pinMode(THin_U,OUTPUT);
-  pinMode(THin_V,OUTPUT);
-  pinMode(THin_W,OUTPUT);
+Serial.begin(2000000);
 
-  timestamp = micros();
+timestamp = micros();
+  // put your main code here, to run repeatedly:
 
 }
+
 
 
 bool lasta, lastb, lastc;
 int i = 0;
 double moyenne = 0;
+int alpha = 128;
 
+
+int valeur_actuelle = 0;
+
+  int test = 0;
 void loop() {
+
     long begintimestamp = micros();
+    
+    a = digitalRead(capteur_a);
+    b = digitalRead(capteur_b);
+    c = digitalRead(capteur_c);
+  
+    
+    K1 = not(a) and (b); //K1 sur preparation
+    if (K1==1) {
+        digitalWrite(Lin_U, 0);
+        digitalWrite(Hin_U, K1);
+    }
+    K2 = a and not(b); //K2 sur preparation
+    if (K2==1) {
+        digitalWrite(Hin_U, 0);
+        digitalWrite(Lin_U, K2);
+    }
+    K3 = c and not(b); //K3 sur preparation
+    if (K3==1) {
+        digitalWrite(Lin_V, 0);
+        digitalWrite(Hin_V, K3);
+    }
+    K4 = not(c) and (b); //K4 sur preparation
+    if (K4==1) {
+        digitalWrite(Hin_V, 0);
+        digitalWrite(Lin_V, K4);
+    }
+    K5 = a and not(c); //K5 sur preparation
+    if (K5==1) {
+        digitalWrite(Lin_W, 0);
+        digitalWrite(Hin_W, K5);
+    }
+    K6 = not(a) and c; //K6 sur preparation
+    if (K6==1) {
+        digitalWrite(Hin_W, 0);
+        digitalWrite(Lin_W, K6);
+    }
+
+    
+    analogWrite(pin_alpha, alpha);
+    
+    if(lasta!=a || lastb!=b || lastc!=c){
+        lasta = a;
+        lastb = b;
+        lastc = c;
+     
+        valeur_actuelle = 2500000/(begintimestamp - timestamp);
+       
+        moyenne = (moyenne * 99 + valeur_actuelle)/100.;  // (1000000 / delai) / 24 * 60
+                                                                               // (1s en us / delai en us) / 24 poles * 60 (conversion rpm)
+        if(i++ >= 50){
+             Serial.print(0);  // échelle de dessin (0 rpm min)
+            Serial.print(" ");
+            
+            Serial.print(moyenne);
+            Serial.print(" ");
+            
+            Serial.print(alpha);
+            Serial.print(" ");
+            Serial.print(1970); // échelle de dessin (10000 rpm max)
+            Serial.print(" ");
+            Serial.print(2030); // échelle de dessin (10000 rpm max)
+            Serial.print(" ");
+            Serial.print(4000); // échelle de dessin (10000 rpm max)
+            Serial.println();
+            i=0;
+        }
+
+        if(moyenne > 2030)
+            alpha = (alpha < 5) ? 0 : (alpha-5);
+        else if(moyenne < 1970)
+            alpha = (alpha > (254-5)) ? (255) : (alpha+5);
+                
+        timestamp = begintimestamp;
+    }
 
   
-  a = digitalRead(Ha);
-  b = digitalRead(Hb);
-  c = digitalRead(Hc);
-
-  kl_U = not(a) and b;
-  kh_U = a and not(b);
-
-  kl_V = not(b) and c;
-  kh_V = b and not(b);
-
-  kl_W = not(c) and a;
-  kh_W = not(a) and c;
-
-  digitalWrite(THin_U,kh_U);
-  digitalWrite(TLin_U,kl_U);
   
-  digitalWrite(THin_V,kh_V);
-  digitalWrite(TLin_V,kl_V);
-  
-  digitalWrite(THin_W,kh_W);
-  digitalWrite(TLin_W,kl_W);
-  
-
-  
-  if(lasta!=a || lastb!=b || lastc!=c){
-      lasta = a;
-      lastb = b;
-      lastc = c;
-
-      moyenne = (moyenne * 99 + 2500000/(begintimestamp - timestamp))/100.;  // (1000000 / delai) / 24 * 60
-                                                                             // (1s en us / delai en us) / 24 poles * 60 (conversion rpm)
-      if(i++ >= 50){
-        Serial.print(0);  // échelle de dessin (0 rpm min)
-        Serial.print(" ");
-        Serial.print(moyenne);
-        Serial.print(" ");
-        Serial.println(10000); // échelle de dessin (10000 rpm max)
-        i=0;
-      }
-      
-      timestamp = begintimestamp;
-
-}}
+  // Serial.print("\r\n");
+}
