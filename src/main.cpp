e4cf9ead221d37dc7d70ec0aea095bfb013dd162
Anas <anas.daggag@gmail.com>
2025-01-17 16:36:44 +0100

    useless

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <Arduino.h>
 
-
 #define Ha 10
 #define Hb 9
 #define Hc 8
@@ -13,13 +12,8 @@
 #define THin_V 3
 #define THin_W 4
 
-#define DCDC 11
-
 bool a, b,c;
 bool kl_U, kh_U, kl_V, kh_V, kl_W, kh_W;
-
-long timestamp;
-
 
 void setup() {
   // put your setup code here, to run once:
@@ -36,22 +30,9 @@ void setup() {
   pinMode(THin_U,OUTPUT);
   pinMode(THin_V,OUTPUT);
   pinMode(THin_W,OUTPUT);
-
-  timestamp = micros();
-
-  digitalWrite(DCDC,128);
-
-
 }
 
-
-bool lasta, lastb, lastc;
-int i = 0;
-double moyenne = 0;
-
 void loop() {
-    long begintimestamp = micros();
-
   
   a = digitalRead(Ha);
   b = digitalRead(Hb);
@@ -65,23 +46,14 @@ void loop() {
 
   kl_W = not(c) and a;
   kh_W = not(a) and c;
+
+  digitalWrite(THin_U,kh_U);
+  digitalWrite(TLin_U,kl_U);
   
-  if(lasta!=a || lastb!=b || lastc!=c){
-      lasta = a;
-      lastb = b;
-      lastc = c;
-
-      moyenne = (moyenne * 99 + 2500000/(begintimestamp - timestamp))/100.;  // (1000000 / delai) / 24 * 60
-                                                                             // (1s en us / delai en us) / 24 poles * 60 (conversion rpm)
-      if(i++ >= 50){
-        Serial.print(0);  // échelle de dessin (0 rpm min)
-        Serial.print(" ");
-        Serial.print(moyenne);
-        Serial.print(" ");
-        Serial.println(10000); // échelle de dessin (10000 rpm max)
-        i=0;
-      }
-      
-      timestamp = begintimestamp;
-
-}}
+  digitalWrite(THin_V,kh_V);
+  digitalWrite(TLin_V,kl_V);
+  
+  digitalWrite(THin_W,kh_W);
+  digitalWrite(TLin_W,kl_W);
+  
+}
