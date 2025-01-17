af7725f6923a0574a400ccde5bdffafca01b366b
cherif <benfetimacherif@gmail.com>
2025-01-17 17:22:53 +0100

    Correcteur de vitesse simple sans PID

--- src/main.cpp
+++ src/main.cpp
@@ -1,142 +1,113 @@
-// Pilotage des phases (sorties)
-#define Hin_U 13
-#define Lin_U 12
-#define Hin_V 11
-#define Lin_V 10
-#define Hin_W 9
-#define Lin_W 8
+#include <Arduino.h>
 
-// Acq Hall
-#define capteur_a 7
-#define capteur_b 6
-#define capteur_c 5
+#define Ha 10
+#define Hb 9
+#define Hc 8
 
-// pilotage alpha
-#define pin_alpha 4
+#define TLin_U 5
+#define TLin_V 6
+#define TLin_W 7
 
-
-#define PAS_ALPHA 5
+#define THin_U 2
+#define THin_V 3
+#define THin_W 4
+#define BUCK 12
 
 bool a, b, c;
-bool K1, K2, K3, K4, K5, K6;
+
+bool kl_U, kh_U, kl_V, kh_V, kl_W, kh_W;
 
 long timestamp;
-void setup() {
-// put your setup code here, to run once:
-// COnfiguration des phases à piloter
-pinMode(Hin_U, OUTPUT);
-pinMode(Lin_U, OUTPUT);
-pinMode(Hin_V, OUTPUT);
-pinMode(Lin_V, OUTPUT);
-pinMode(Hin_W, OUTPUT);
-pinMode(Lin_W, OUTPUT);
 
-// Configuration de l'acq. des capteurs Hall
-pinMode(capteur_a, INPUT);
-pinMode(capteur_b, INPUT);
-pinMode(capteur_c, INPUT);
-
-// Configuration
-pinMode(pin_alpha, OUTPUT);
-
-
-Serial.begin(2000000);
-
-timestamp = micros();
-  // put your main code here, to run repeatedly:
-
-}
-
-
+int alpha = 128;  // Valeur initiale du rapport cyclique du buck (entre 0 et 255)
+double vitesse_consigne = 3000;  // Vitesse de consigne en RPM (ajustez cette valeur selon vos besoins)
 
 bool lasta, lastb, lastc;
 int i = 0;
-double moyenne = 0;
-int alpha = 128;
+double moyenne = 0;  // Vitesse moyenne (RPM)
+long dernierDelai = 0;
 
+void setup() {
+    // put your setup code here, to run once:
+    Serial.begin(2000000);
+    pinMode(Ha, INPUT);
+    pinMode(Hb, INPUT);
+    pinMode(Hc, INPUT);
+    pinMode(BUCK, OUTPUT);
 
-int valeur_actuelle = 0;
+    pinMode(TLin_U, OUTPUT);
+    pinMode(TLin_V, OUTPUT);
+    pinMode(TLin_W, OUTPUT);
 
-  int test = 0;
+    pinMode(THin_U, OUTPUT);
+    pinMode(THin_V, OUTPUT);
+    pinMode(THin_W, OUTPUT);
+
+    timestamp = micros();
+}
+
 void loop() {
-
     long begintimestamp = micros();
-    
-    a = digitalRead(capteur_a);
-    b = digitalRead(capteur_b);
-    c = digitalRead(capteur_c);
-  
-    
-    K1 = not(a) and (b); //K1 sur preparation
-    if (K1==1) {
-        digitalWrite(Lin_U, 0);
-        digitalWrite(Hin_U, K1);
-    }
-    K2 = a and not(b); //K2 sur preparation
-    if (K2==1) {
-        digitalWrite(Hin_U, 0);
-        digitalWrite(Lin_U, K2);
-    }
-    K3 = c and not(b); //K3 sur preparation
-    if (K3==1) {
-        digitalWrite(Lin_V, 0);
-        digitalWrite(Hin_V, K3);
-    }
-    K4 = not(c) and (b); //K4 sur preparation
-    if (K4==1) {
-        digitalWrite(Hin_V, 0);
-        digitalWrite(Lin_V, K4);
-    }
-    K5 = a and not(c); //K5 sur preparation
-    if (K5==1) {
-        digitalWrite(Lin_W, 0);
-        digitalWrite(Hin_W, K5);
-    }
-    K6 = not(a) and c; //K6 sur preparation
-    if (K6==1) {
-        digitalWrite(Hin_W, 0);
-        digitalWrite(Lin_W, K6);
-    }
 
-    
-    analogWrite(pin_alpha, alpha);
-    
-    if(lasta!=a || lastb!=b || lastc!=c){
+    // Lire les capteurs Hall
+    a = digitalRead(Ha);
+    b = digitalRead(Hb);
+    c = digitalRead(Hc);
+
+    // Logique des phases du moteur
+    kl_U = not(a) and b;
+    kh_U = a and not(b);
+
+    kl_V = not(b) and c;
+    kh_V = b and not(b);
+
+    kl_W = not(c) and a;
+    kh_W = not(a) and c;
+
+    digitalWrite(THin_U, kh_U);
+    digitalWrite(TLin_U, kl_U);
+  
+    digitalWrite(THin_V, kh_V);
+    digitalWrite(TLin_V, kl_V);
+  
+    digitalWrite(THin_W, kh_W);
+    digitalWrite(TLin_W, kl_W);
+
+    // Calcul de la vitesse
+    if (lasta != a || lastb != b || lastc != c) {
         lasta = a;
         lastb = b;
         lastc = c;
-     
-        valeur_actuelle = 2500000/(begintimestamp - timestamp);
-       
-        moyenne = (moyenne * 99 + valeur_actuelle)/100.;  // (1000000 / delai) / 24 * 60
-                                                                               // (1s en us / delai en us) / 24 poles * 60 (conversion rpm)
-        if(i++ >= 50){
-             Serial.print(0);  // échelle de dessin (0 rpm min)
-            Serial.print(" ");
-            
-            Serial.print(moyenne);
-            Serial.print(" ");
-            
-            Serial.print(alpha);
-            Serial.print(" ");
-            Serial.print(1970); // échelle de dessin (10000 rpm max)
-            Serial.print(" ");
-            Serial.print(2030); // échelle de dessin (10000 rpm max)
-            Serial.print(" ");
-            Serial.print(4000); // échelle de dessin (10000 rpm max)
-            Serial.println();
-            i=0;
+
+        double delai = (begintimestamp - timestamp);  // Calcul du délai en microsecondes
+        if (delai > 0) {
+            moyenne = (moyenne * 99 + 2500000.0 / delai) / 100.0;  // Calcul de la vitesse moyenne (RPM)
         }
 
-        if(moyenne > 2030)
-            alpha = (alpha < 5) ? 0 : (alpha-5);
-        else if(moyenne < 1970)
-            alpha = (alpha > (254-5)) ? (255) : (alpha+5);
-                
-        timestamp = begintimestamp;
-    }
+        // Asservissement de la vitesse
+        if (moyenne < vitesse_consigne) {
+            // Si la vitesse est trop faible, augmenter le rapport cyclique
+            alpha = min(alpha + 1, 255);  // Limiter à 255 (maximum PWM)
+        } else if (moyenne > vitesse_consigne) {
+            // Si la vitesse est trop grande, diminuer le rapport cyclique
+            alpha = max(alpha - 1, 0);  // Limiter à 0 (minimum PWM)
+        }
 
-  
-  
-  // Serial.print("\r\n");
+        // Appliquer la commande PWM au BUCK
+        analogWrite(BUCK, alpha);
+
+        if (i++ >= 50) {
+            // Afficher les informations de débogage toutes les 50 itérations
+            Serial.print(0);  // échelle de dessin (0 RPM min)
+            Serial.print(" ");
+            Serial.print(moyenne);  // Afficher la vitesse actuelle
+            Serial.print(" ");
+            Serial.print(10000);  // échelle de dessin (10000 RPM max)
+            Serial.print(" alpha: ");
+            Serial.println(alpha);  // Afficher la valeur actuelle du rapport cyclique (alpha)
+            i = 0;
+        }
+
+        timestamp = begintimestamp;  // Mettre à jour le timestamp pour le prochain calcul
+    }
 }
